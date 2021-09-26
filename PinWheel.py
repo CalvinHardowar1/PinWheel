72c59be80e4c1d5f309566bd80dcb9fc2247daee
Calvin Hardowar <36016757+CalvinHardowar1@users.noreply.github.com>
2021-09-26 01:32:27 -0400

    Add files via upload

---
+++ PinWheel.py
@@ -0,0 +1,37 @@
+
+import turtle
+turtle2=turtle.Screen()
+turtle1=turtle.Turtle()
+turtle1.shape('arrow')
+turtle1.color("purple")
+turtle1.pensize(3)
+turtle1.forward(30)
+
+for b in range(3):
+    turtle1.forward(50)
+    turtle1.right(120)
+turtle1.back(30)
+turtle1.right(90)
+turtle1.forward(30)
+
+for b in range(3):
+    turtle1.forward(50)
+    turtle1.right(120)
+turtle1.back(30)
+turtle1.right(90)
+turtle1.forward(30)
+
+for b in range(3):
+    turtle1.forward(50)
+    turtle1.right(120)
+turtle1.back(30)
+turtle1.right(90)
+turtle1.forward(30)
+
+for b in range(3):
+    turtle1.forward(50)
+    turtle1.right(120)
+
+turtle1.back(30)
+
+turtle2.exitonclick()
